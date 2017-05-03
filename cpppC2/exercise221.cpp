9a4498708ce87996d85150d55d010f2def4d990f
SSRockySS <402625942@qq.com>
2017-05-03 23:53:44 +0800

    Add test narrowing

--- cpppC2/exercise221.cpp
+++ cpppC2/exercise221.cpp
@@ -6,6 +6,11 @@ int main(void)
 	int u3{2};
 	int u4(3);
 	std::cout<<u1<<u2<<u3<<u4<<std::endl;
+    
+	long double ld = 3.1415926536;
+	int a{ld}, b = {ld};
+	int c(ld), d = ld;
+	std::cout<<a<<b<<c<<d<<std::endl;
 
 	return 0;
 }
