010329022b5a252ea85f5d66b8debc87221a9a72
SSRockySS <402625942@qq.com>
2017-05-27 20:08:45 +0800

    maybe i forgot this......

---
+++ cpppC4/e491.cpp
@@ -0,0 +1,11 @@
+/*sizeof*/
+#include<iostream>
+using namespace std;
+int main(void)
+{
+	cout<<sizeof(int)<<" "<<sizeof(float)<<" "<<sizeof(double)<<" "
+		<<sizeof(short)<<" "<<sizeof(long)<<" "<<sizeof(long long)<<" "
+		<<sizeof(char)<<" "<<sizeof(bool)<<" "<<sizeof(long double)<<endl;
+
+	return 0;
+}
